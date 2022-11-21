27a1c3956f0d0be595579c7c17c54d0366c312cd
LukBartsch <lukasz.bartsch@gmail.com>
2022-11-21 21:49:39 +0100

    Cleanong of files

--- layouts.py
+++ layouts.py
@@ -1,9 +1,6 @@
-import dash_table
-import dash_core_components as dcc
+
 import dash_html_components as html
 
-from datetime import datetime
-from datetime import timedelta
 
 # from common import COLORS
 # from common import BASE_CURRENCIES
