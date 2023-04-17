8f7e20bc30fc58fac7d4b8c452a8f0e75acb7e3c
LukBartsch <lukasz.bartsch@gmail.com>
2023-04-17 19:46:59 +0200

    Add auto size table

--- layout/main_layout.py
+++ layout/main_layout.py
@@ -1,5 +1,6 @@
 
 from dash import html, dcc
+import dash_bootstrap_components as dbc
 
 from layout.crypto_graph_section import main_crypto_title, crypto_and_date_section, crypto_graph
 from layout.fear_and_greed_index import fng_index_table, fng_index_line_graph, fng_index_info
@@ -25,7 +26,8 @@ layout = html.Div(className="main", children=[
             
             crypto_amount,
             warning_message,
-            current_prices_table
+            current_prices_table,
+     
         ],
         style={
                 'backgroundColor': 'rgb(50, 50, 50)',
